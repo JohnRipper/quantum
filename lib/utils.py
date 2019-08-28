8099d794e2c8b752661a4ae7fa51e8e96333aab5
john <johnripper5150@gmail.com>
2019-08-28 05:46:00 +0000

    methods for simple data storage

--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,10 @@
 import ast
 import inspect
+import os
+
+from lib.logging import QuantumLogger
+
+logger = QuantumLogger("utils")
 
 def get_decorators(cls):
     target = cls
@@ -21,3 +26,25 @@ def get_decorators(cls):
     node_iter.visit(ast.parse(inspect.getsource(target)))
     return decorators
 
+
+def string_in_file(file: str, search_term: str):
+    with open(f"./../data/app_data/{file}") as data_file:
+        data_file.seek(0, 0)
+        for cnt, line in enumerate(data_file):
+            logger.debug(f"{cnt}: Checking for {search_term} inside {file}:{line}")
+            if search_term == line:
+                data_file.close()
+                logger.debug(f"{cnt}: Found {search_term} inside {file}:{line}")
+                return True
+        logger.debug(f"Not found")
+        data_file.close()
+        return False
+
+
+def append_string_in_file(file: str, search_term: str):
+    with open(f"./../data/app_data/{file}", "a") as data_file:
+        data_file.seek(0, 2)
+        data_file.writelines(search_term)
+        data_file.flush()
+        logger.debug(f"{search_term} added to {file} in /data/app_data")
+        data_file.close()
