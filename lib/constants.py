6ba380e9e8564c218acc49bc1f500bcabeb9026a
john <johnripper5150@gmail.com>
2019-08-30 02:30:02 +0000

    added more events

--- lib/constants.py
+++ lib/constants.py
@@ -1,23 +1,24 @@
 class SocketEvents:
-    MSG = 'msg'
-    CAPTCHA = 'captcha'
-    USERLIST = 'userlist'
-    JOIN = 'join'
-    JOINED = 'joined'
-    QUIT = 'quit'
-    PING = 'ping'
-    YUT_STOP = 'yut_stop'
-    PVTMSG = 'pvtmsg'
-    ICESERVERS = 'iceservers'
-    PUBLISH = 'publish'
-    UNPUBLISH = 'unpublish'
-    STREAM_CONNECTED = 'stream_connected'
-    STREAM_CLOSED = 'stream_closed'
-    SDP = 'sdp'
+    MSG = "msg"
+    CAPTCHA = "captcha"
+    CLOSED = "closed"
+    USERLIST = "userlist"
+    JOIN = "join"
+    JOINED = "joined"
+    QUIT = "quit"
+    PING = "ping"
+    YUT_STOP = "yut_stop"
+    PVTMSG = "pvtmsg"
+    ICESERVERS = "iceservers"
+    PUBLISH = "publish"
+    UNPUBLISH = "unpublish"
+    STREAM_CONNECTED = "stream_connected"
+    STREAM_CLOSED = "stream_closed"
+    SDP = "sdp"
 
     ALL = [MSG, CAPTCHA, USERLIST, JOIN, JOINED, QUIT, PING, YUT_STOP,
            PVTMSG, ICESERVERS, PUBLISH, UNPUBLISH, STREAM_CONNECTED,
-           STREAM_CLOSED, SDP]
+           STREAM_CLOSED, SDP, CLOSED]
 
 
 class AppData:
