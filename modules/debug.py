9a523bf8fa189009b7867f96b32088e42c862d1b
john <johnripper5150@gmail.com>
2019-08-30 02:31:33 +0000

    reloading command now works

--- modules/debug.py
+++ modules/debug.py
@@ -16,6 +16,7 @@ class Debug(Cog):
 
     @makeCommand(name="reload", description="<cog_name> reloads a cog")
     async def reload(self,  c: Command):
-        self.bot.remove_cog(c.message)
-        self.bot.add_cog(c.message)
-        await self.bot.send_message(f"reloaded {c.message} Cog")
+        self.bot.remove_cog(c.message.rstrip())
+
+        self.bot.add_cog(c.message.rstrip())
+        await self.bot.send_message(f"reloaded {c.message.rstrip()} Cog")
