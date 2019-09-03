ea8b2b10d79431bd010f9102982b5caf5bb0fb18
aida <Autotonic@users.noreply.github.com>
2019-09-03 18:26:41 -0400

    whoops, split up autourl a bit

--- modules/autourl.py
+++ modules/autourl.py
@@ -16,20 +16,25 @@ class AutoUrl(Cog):
         msg = data["text"]
         match = re.findall(self.pattern, msg)
         # workaround for youtube playing
+        print(self.settings["ignores"])
         if data["handle"] == self.bot.handle or re.match("\A.?play", msg):
             pass
-        # TODO sgetti
-        elif len(self.settings["ignores"]) >= 1:
-            for ignore in self.settings["ignores"]:
-                if re.match(ignore, msg):
-                    pass
-                else:
-                    await self.bot.send_message(await self.get_title(match[0]))
-                    break
         elif msg.startswith(self.exclusion_char) or len(match) == 0:
             pass
         else:
-            await self.bot.send_mesaage(await self.get_title(match[0]))
+            if self.ignore_msg(match[0]) is False:
+                await self.bot.send_message(await self.get_title(match[0]))
+
+    def ignore_msg(self, msg):
+        print(msg)
+        if len(self.settings["ignores"]) >= 1:
+            for ignore in self.settings["ignores"]:
+                if bool(re.findall(ignore, msg)):
+                    return True
+            else:
+                return False
+        else:
+            return False
 
     async def get_title(self, url):
         res = requests.get(url)
