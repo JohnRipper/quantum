56a2354b13fd261cab28463bfbaa61738df4a77e
aida <Autotonic@users.noreply.github.com>
2019-09-03 17:30:24 -0400

    sgetti up autourl

--- modules/autourl.py
+++ modules/autourl.py
@@ -18,22 +18,27 @@ class AutoUrl(Cog):
         # workaround for youtube playing
         if data["handle"] == self.bot.handle or re.match("\A.?play", msg):
             pass
+        # TODO sgetti
         elif len(self.settings["ignores"]) >= 1:
             for ignore in self.settings["ignores"]:
                 if re.match(ignore, msg):
                     pass
+                else:
+                    await self.bot.send_message(await self.get_title(match[0]))
+                    break
         elif msg.startswith(self.exclusion_char) or len(match) == 0:
             pass
         else:
-            res = requests.get(match[0])
-            soup = bs4(requests.get(match[0]).text, "html.parser")
-            if soup != None:
-                # TODO maybe split at "/" to do like [URL Title] - site.url
-                # maybe handle 404, meh
-                try:
-                    title = soup.title.string.strip()
-                except AttributeError:
-                    pass
-                else:
-                    await self.bot.send_message(f"{soup.title.string}")
+            await self.bot.send_mesaage(await self.get_title(match[0]))
+
+    async def get_title(self, url):
+        res = requests.get(url)
+        soup = bs4(res.text, "html.parser")
+        if soup != None:
+            try:
+                title = soup.title.string
+            except AttributeError as error:
+                self.bot.warning(erro)
+            else:
+                return title.strip()
 
