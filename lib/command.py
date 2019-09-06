91b73623db656f06b143816291deb5b5787833f6
john <johnripper5150@gmail.com>
2019-09-06 02:21:23 +0000

    added sender account object to command.

--- lib/command.py
+++ lib/command.py
@@ -1,3 +1,5 @@
+from lib.account import Account
+
 def makeCommand(name, description, cls=None, **attrs):
     def wrap(f):
         if isinstance(f, Command):
@@ -9,16 +11,29 @@ def makeCommand(name, description, cls=None, **attrs):
     return wrap
 
 
+def restrictTo(role: (str, int), cls=None, **attrs):
+    def wrap(f):
+        if isinstance(f, Command):
+            raise TypeError('Callback is already an instance.')
+        f.role = role
+        return f
+
+    return wrap
+
+
 class Command:
-    def __init__(self, data, bot):
+    def __init__(self, data, bot, account: Account):
+        self._bot = bot
         self.raw_data = data
         self.command, self.message = f'{data["text"]}{" "}'.split(' ', 1)
+        self.prefix = self.command[0]
         self.command = self.command[1:]
+        self.account = account
+        # depreciating in favor of lib.Account.
         self.sender = bot.handle_to_name(data["handle"])
-        self._bot = bot
 
-    def send_message(self, message: str):
-        self._bot.send_message(message)
+    async def send_message(self, message: str):
+        await self._bot.send_message(message)
 
     def send_private_message(self, message: str):
         return
