977dc25a78c594f396c31afed35a2fd684cbefab
john <johnripper5150@gmail.com>
2019-10-17 00:13:32 +0000

    mostly redundant since bot.py was updated with the new logger. Keeping around for gist/private server posting of logs.

--- modules/log.py
+++ modules/log.py
@@ -1,15 +1,14 @@
 from lib.cog import Cog
 from lib.command import makeCommand, Command
 
-class Log(Cog):
 
+class Log(Cog):
     #############################
     # Commands
     #############################
     @makeCommand(name='printlog' , description='<-c> <-d> creates a copy of a Chat/debug log ')
     async def log_to_pastebin(self, c: Command):
         if c.message.startswith("-c"):
-
             await self.bot.send_message('not implemented for chat logs yet')
 
         if c.message.startswith("-d"):
