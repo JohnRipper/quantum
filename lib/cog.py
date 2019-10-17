018d4f630c63876c54e36c1420153bc83204af13
john <johnripper5150@gmail.com>
2019-10-17 00:07:09 +0000

    added helper methods for communicating with websocket

--- lib/cog.py
+++ lib/cog.py
@@ -1,4 +1,6 @@
 from lib.qlogging import QuantumLogger
+import json
+from lib.constants import SocketEvents
 
 
 class Cog:
@@ -6,6 +8,41 @@ class Cog:
         self.name = self.__class__.__name__
         self.logger = QuantumLogger(self.name)
         self.bot = bot
+
+    #############################
+    # Helper methods
+    #############################
+    async def send_message(self, message: str):
+        await self.bot.send_message(message)
+
+    async def send_private_message(self, message: str):
+        await self.bot.send_private(message)
+
+    async def send_ws(self, data: dict):
+        await self.bot.wsend(json.dumps(data))
+
+    def get_req(self):
+        return self.bot.get_req()
+
+    async def do_ban(self, username):
+        # look at ban command, does it use handle or username?
+        return
+
+    async def do_kick(self, username):
+        return
+
+    async def change_nick(self, nickname: str):
+        data = {"nick": nickname,
+                "req": self.get_req(),
+                "tc": SocketEvents.NICK}
+        return self.send_ws(data)
+
+    async def get_account(self, handle):
+        return self.bot.accounts[handle]
+
+    #############################
+    # Events
+    #############################
 
     async def closed(self, data):
         # tc closed the connnection
@@ -35,8 +72,56 @@ class Cog:
         # youtube has been closed
         return
 
-    async def yut_start(self, data):
-        # youtube has been started
+    async def yut_pause(self, data):
+        # Youtube video paused
+
+        # data example
+        # {"handle": 3468338,
+        #  "item": {"duration": 315,
+        #           "id": "B-oU2xlViRQ",
+        #           "image": "",
+        #           "offset": 77.243339538574219,
+        #           "playlist": False,
+        #           "title": "Megadeth - Hangar 18 (HD) — [MegaHermansen]"}, "req": 425,
+        #  "success": True, "tc": "yut_pause"}
+        return
+
+    async def yut_play(self, data):
+        # youtube has been played, or seeked
+
+        # data examples
+
+        # play
+        # {"handle": 3468338,
+        #  "item": {"duration": 315,
+        #           "id": "B-oU2xlViRQ",
+        #           "image": "",
+        #           "offset": 31.102100372314453,
+        #           "playlist": False,
+        #           "title": "Megadeth - Hangar 18 (HD) — [MegaHermansen]"},
+        #  "req": 422,
+        #  "success": True,
+        #  "tc": "yut_play"}
+
+        #  seek
+        # {"tc": "yut_play",
+        #  "req": 426,
+        #  "item": {"id": "B-oU2xlViRQ",
+        #           "duration": 311.021,
+        #           "offset": 77.24333953857422,
+        #           "seek": True}}
+        return
+
+    async def nick(self, data):
+        # nickname changed for specified user.
+
+        # data example
+
+        # {"handle": 3468338,
+        #  "nick": "_johan_muted",
+        #  "req": 484,
+        #  "success": True,
+        #  "tc": "nick"}
         return
 
     async def msg(self, data):
@@ -86,4 +171,3 @@ class Cog:
         # - set to remote description
 
         return
-
