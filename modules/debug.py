04d25d23432cd8a6cc3c6fc9c64d9e857716f0c5
john <johnripper5150@gmail.com>
2019-10-19 10:18:09 +0000

    added load and unload. untested. prob wont work.

--- modules/debug.py
+++ modules/debug.py
@@ -13,8 +13,34 @@ class Debug(Cog):
 
     @makeCommand(name="reload", description="<cog_name> reloads a cog")
     async def reload(self,  c: Command):
-        self.bot.remove_cog(c.message.rstrip())
-        self.bot.reload_cog(c.message.rstrip())
-        self.bot.add_cog(c.message.rstrip())
-        self.logger.debug(f"Reloaded module: {c.message.rstrip()}")
-        await self.bot.send_message(f"Reloaded module: {c.message.rstrip()}")
+        module = c.message.rstrip()
+        self.bot.remove_cog(module)
+        self.bot.reload_cog(module)
+        self.bot.add_cog(module)
+        self.logger.debug(f"Reloaded module: {module}")
+        await self.bot.send_message(f"Reloaded module: {module}")
+
+    @makeCommand(name="load", description="<cog_name> reloads a cog")
+    async def load(self, c: Command):
+        module = c.message.rstrip()
+        if module in self.bot.modules:
+            await self.send_message("Already loaded")
+            return
+        self.bot.remove_cog(module)
+        botmsg = f"Reloaded module: {module}"
+        self.logger.debug(botmsg)
+        await self.bot.send_message(botmsg)
+
+    @makeCommand(name="unload", description="<cog_name> reloads a cog")
+    async def unload(self, c: Command):
+        module = c.message.rstrip()
+        if module not in self.bot.modules:
+            await self.send_message(f"{module} cannot be unloaded")
+            return
+        self.bot.add_cog(module)
+        self.logger.debug(f"unloaded module: {module}")
+        await self.send_message(f"unloaded module: {module}")
+
+    @makeCommand(name="modules", description="prints a list of modules")
+    async def modules(self, c: Command):
+        await self.send_message(self.bot.settings["bot"]["modules"])
