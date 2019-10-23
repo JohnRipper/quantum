9d03297783de0a2415850e7bec2d19c5bf14e161
john <johnripper5150@gmail.com>
2019-10-23 10:49:34 +0000

    changed to dataclass.

--- lib/account.py
+++ lib/account.py
@@ -1,29 +1,30 @@
 from lib.constants import AppData, Role
 from lib.utils import append_string_in_file, string_in_file
+from dataclasses import dataclass
 
 
+@dataclass
 class Account:
-    role: (str, int)
+    # "userlist" and "join" events use the same user object.
 
-    def __init__(self, data):
-        # "userlist" and "join" events use the same user object.
-        self.raw_data = data
-        # convert raw data to class objects
-        self.achievement_url = data["achievement_url"]
-        self.avatar = data["avatar"]
-        self.featured = data["featured"]
-        self.giftpoints = data["giftpoints"]
-        self.handle = data["handle"]
-        self.lurker = data["lurker"]
-        self.mod = data["mod"]
-        self.nick = data["nick"]
-        self.owner = data["owner"]
-        self.session_id = data["session_id"]
-        self.subscription = data["subscription"]
+    # tc object
+    achievement_url: str
+    avatar: str
+    featured: bool
+    giftpoints: int
+    handle: int
+    lurker: bool
+    mod: bool
+    nick: str
+    owner: bool
+    session_id: str
+    subscription: bool
+    username: str
 
-        self.username = data["username"]
+    # custom fields
+    role: (str, int) = Role.NONE
 
-        self.role = Role.NONE
+    def __post_init__(self):
         # check in order to ensure user gets highest available role assigned.
         if self.is_op():
             self.role = Role.OP
