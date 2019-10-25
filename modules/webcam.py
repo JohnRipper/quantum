a5825919be63a4481ff37e81e06d5a9c2f1d3082
john <johnripper5150@gmail.com>
2019-10-25 07:56:09 +0000

    fixed. Thanks @Autotonic

--- modules/webcam.py
+++ modules/webcam.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+from dataclasses import dataclass
+
 import aioice
 from aiortc.contrib.media import MediaPlayer
 from aiortc import (
@@ -8,10 +10,6 @@ from aiortc import (
 )
 from lib.cog import Cog
 from lib.command import makeCommand, Command
-from threading import Thread
-
-
-
 
 
 
@@ -20,7 +18,7 @@ class Webcam(Cog):
         super().__init__(bot)
         self.candidates = []
         self.pc = RTCPeerConnection()
-        self.connection = aioice.Connection(ice_controlling=True)
+        self.connection = aioice.Connection(ice_controlling=True, )
         self.stun_server = None
         self.cammed = False
 
@@ -30,8 +28,11 @@ class Webcam(Cog):
 
             if self.pc.iceConnectionState == "failed":
                 await self.pc.close()
-            if self.pc.iceGatheringState == 'complete':
-                return
+
+            if self.pc.iceConnectionState == 'completed':
+                print("completed")
+
+
 
     #############################
     # Events
@@ -43,7 +44,8 @@ class Webcam(Cog):
         await self.offer(data)
 
     async def publish(self, data):
-        await self.subscribe(data['handle'])
+        # cawe dont await self.subscribe(data['handle'])
+        return
 
     async def unpublish(self, data):
         return
@@ -61,10 +63,13 @@ class Webcam(Cog):
                     await self.pc.setRemoteDescription(d)
                     self.connection.remote_password = ""
                     self.connection.remote_username = ""
-                    await self.connection.connect()
+                    asyncio.ensure_future(self.connection.connect(), loop=asyncio.get_event_loop())
+
+
 
     async def stream_closed(self, data):
-        await self.bot.wsend(json.dumps({"tc": "subscribe", "req": 2, "handle": data['handle']}))
+        # await self.bot.wsend(json.dumps({"tc": "subscribe", "req": 2, "handle": data['handle']}))
+        return
 
     async def stream_connected(self, data):
         return
@@ -75,27 +80,41 @@ class Webcam(Cog):
     @makeCommand(name='cam' , description='attempts to cam up')
     async def cam(self, c: Command):
         # get ice servers
-        await self.bot.wsend(json.dumps({"tc": "getice", "req": 216}))
+        await self.bot.wsend(json.dumps({"tc": "getice", "req": self.get_req()}))
+
+    @makeCommand(name='endcam', description='cams down')
+    async def endcam(self, c: Command):
+        # get ice servers
+        await self.bot.wsend(json.dumps({"handle": self.bot.handle, "tc": "unpublish"}))
 
     #############################
     # class methods
     #############################
 
     async def close_stream(self, handle_id):
-        self.bot.wsend(json.dumps({"tc":"stream_close","req":215,"handle": handle_id}))
+        self.bot.wsend(json.dumps({"tc": "stream_close", "req": self.get_req(), "handle": handle_id}))
 
     async def subscribe(self, handle_id):
-        await self.bot.wsend(json.dumps({"tc":"subscribe","req":2,"handle": handle_id}))
+        if handle_id != self.bot.handle:
+            await self.bot.wsend(json.dumps({"tc": "subscribe", "req": self.get_req(), "handle": handle_id}))
 
     async def offer(self, data):
         s_server = data['servers'][0].split(":")
+
         STUN_SERVER = (s_server[1], int(s_server[2]))
         self.connection.stun_server = STUN_SERVER
         self.stun_server = STUN_SERVER
         await self.connection.gather_candidates()
 
-        options = {"framerate": "30", "video_size": "1920x1080"}
-        player = MediaPlayer("/dev/video0", format="v4l2", options=options)
+        # using a physical cam, almost no lag.
+        # options = {"volume": "33"}
+        # player = MediaPlayer("/dev/video0", format="v4l2", options=options)
+
+        # screen grab. laggy.
+        options = {"volume": "33", "video_size": "640x480"}
+        player = MediaPlayer("/dev/video2", format="v4l2", options=options)
+
+
         if player and player.audio:
             self.pc.addTrack(player.audio)
         if player and player.video:
@@ -104,7 +123,7 @@ class Webcam(Cog):
         await self.pc.setLocalDescription(await self.pc.createOffer())
         data = {
             "tc": "sdp",
-            "req": 420,
+            "req": self.get_req(),
             "type": "offer",
             "sdp": self.pc.localDescription.sdp,
             "handle": 0
@@ -114,7 +133,7 @@ class Webcam(Cog):
         for c in self.connection.local_candidates:
             data = {
                 "tc": "trickle",
-                "req": 420,
+                "req": self.get_req(),
                 "candidate": "candidate:" + aioice.Candidate.to_sdp(c),
                 "handle": self.bot.handle
             }
@@ -122,7 +141,4 @@ class Webcam(Cog):
             await self.bot.wsend(json.dumps(data))
 
     async def get_ice(self):
-        await self.bot.wsend(json.dumps({'tc': 'getice', 'req':1}))
-
-
-
+        await self.bot.wsend(json.dumps({'tc': 'getice', 'req': self.get_req()}))
