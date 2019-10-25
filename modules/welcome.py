d24131f5353280a4181f2536b574c33526ee14ce
john <johnripper5150@gmail.com>
2019-10-25 08:26:31 +0000

    incorporated the welcome message into the settings.

--- modules/welcome.py
+++ modules/welcome.py
@@ -1,34 +1,31 @@
 from lib.cog import Cog
-import json
-
-from lib.command import makeCommand, Command
 
 
 class Welcome(Cog):
     def __init__(self, bot):
         super().__init__(bot)
-        self.roomname = self.bot.settings["room"]["roomname"]
-        self.settings = self.bot.settings["module"]["welcome"]
+        self.room_name = self.bot.settings["room"]["room_name"]
         self.welcome_message = self.settings["message"]
 
     async def joined(self, data):
-        if data["self"]["owner"]:
-            await self.send_message("I am the boss and im back")
-        else:
-            message = f"Hello everybody I am back. {data['self']['handle']}"
-            await self.send_message(message)
+        if self.settings["self_message"]:
+            if data["self"]["owner"]:
+                await self.send_message("I am the boss and im back")
+            else:
+                message = f"Hello everybody I am back. {data['self']['handle']}"
+                await self.send_message(message)
 
-    # TODO option to disable for guests/non-mods or something
+    # todo terrible.
     async def join(self, data):
         if data["owner"]:
-            await self.bot.send_message("Welcome back boss")
+            await self.send_message("Welcome back boss")
         else:
             message = self.welcome_message
-            if len(message) >= 1: # account for a blank space
+            if len(message) >= 1:
                 message = message.format(**data)
                 if "{room}" in message:
-                    message = message.format(room=self.roomname)
-                await self.send_message(message)
+                    message = message.format(room=self.room_name)
+                    await self.send_message(message)
             else:
                 pass
 
