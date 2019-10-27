74f799fdf2ee5970e9f7bcf2dd6b26d8a2ae2919
Aida <Autotonic@users.noreply.github.com>
2019-10-27 13:03:17 -0400

    some comments in wikipedia, add limit

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -16,6 +16,7 @@ class Wikipedia(Cog):
         wikipedia.set_lang(self.settings["language"])
         sentences = self.settings["sentences"]
         query = c.message
+        # expected response for choosing a topic from `self.searches`
         if len(query) == 1 and query.isdigit():
             query = self.searches[int(query)]
         elif len(query) == 0:
@@ -27,11 +28,14 @@ class Wikipedia(Cog):
             await self.send_message("PageError, FIXME")
         except wikipedia.exceptions.DisambiguationError as err:
             self.searches = err.options[:10]
+            # iterate over self.searches to create a formated string,
+            # ie "0) List Index 0\n1) List Index 1" etc
             fmtd = "\n".join([str(i)+") "+m for i,m in enumerate(self.searches)])
             await self.send_message("Select one with \"wiki #\"\n{}".format(fmtd), clean=False)
         else:
-            # gross clean up
             url = page.url
+            # wikipedia lib reformats headings? yuck.
             summary = re.sub("==.+==", " ", summary)
             summary = re.sub("\n", "", summary)
-            await self.send_message("{}\n{}".format(url,summary), clean=False)
+            # TODO maybe pull this limit from the settings
+            await self.send_message("{}\n{}".format(url,summary), clean=False, limit=1)
