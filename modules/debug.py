ed1a73bae5f968daaa5cfc2cbad66707ea944559
john <johnripper5150@gmail.com>
2019-10-28 16:53:31 +0000

    fixed load and unload

--- modules/debug.py
+++ modules/debug.py
@@ -22,27 +22,26 @@ class Debug(Cog):
 
     @makeCommand(name="load", description="<cog_name> reloads a cog")
     async def load(self, c: Command):
-        # Not working, use reload.
-        module = c.message.rstrip()
-        if module in self.bot.modules:
-            await self.send_message("Already loaded")
-            return
-        self.bot.remove_cog(module)
-        botmsg = f"Reloaded module: {module}"
+        module = self.bot.load_module(c.message)
+        self.bot.add_cog(c.message)
+        botmsg = f"loaded module: {module}"
         self.logger.debug(botmsg)
         await self.send_message(botmsg)
 
     @makeCommand(name="unload", description="<cog_name> reloads a cog")
     async def unload(self, c: Command):
         # Not working, use reload.
-        module = c.message.rstrip()
-        if module not in self.bot.modules:
-            await self.send_message(f"{module} cannot be unloaded")
-            return
-        self.bot.add_cog(module)
-        self.logger.debug(f"unloaded module: {module}")
-        await self.send_message(f"unloaded module: {module}")
+        module = self.bot.load_module(c.message)
+        if module in self.bot.modules:
+            self.bot.modules.remove(module)
+            self.bot.remove_cog(c.message)
+            self.logger.debug(f"unloaded module: {module}")
+            await self.send_message(f"unloaded module: {module}")
 
     @makeCommand(name="modules", description="prints a list of modules")
     async def modules(self, c: Command):
-        await self.send_message(str(self.bot.settings["bot"]["modules"]))
+        await self.send_message(str(self.bot.modules))
+
+    @makeCommand(name="cogs", description="prints a list of modules")
+    async def cogs(self, c: Command):
+        await self.send_message(str(self.bot.cogs.__repr__()))
